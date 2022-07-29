57c05a7576567c7a041b09cf652ab0258f32266c
iCpplus <56351093+iCpplus@users.noreply.github.com>
2022-07-29 08:48:12 +0000

    。

--- src/html.js
+++ src/html.js
@@ -16,7 +16,7 @@ const HTML = function({
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no, shrink-to-fit=no" />
         {headComponents}
       </head>
       <body {...bodyAttributes} className="light">
