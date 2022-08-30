83e008b8e97227333676ca7525721a529abf1d96
iCpplus <56351093+iCpplus@users.noreply.github.com>
2022-08-30 07:19:16 +0000

    title

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const site = {
   pathPrefix: '/gatsby-simple-blog',
-  title: 'space of nana\'s coco',
+  title: 'coco\'s space',
   author: 'Kou ShiXiang',
   description: '一个记录知识和生活的神秘小空间',  
   siteUrl: 'https://gatsby-simple-blog.thundermiracle.com/',
