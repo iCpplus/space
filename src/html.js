afaaa2482bdbd837f819e106a829f8f3af6eb65d
anyscript <anyscript>
2023-06-01 09:58:32 +0800

    百度搜索

--- src/html.js
+++ src/html.js
@@ -14,6 +14,7 @@ const HTML = function ({
   return (
     <html lang="en" {...htmlAttributes}>
       <head>
+        <meta name="baidu-site-verification" content="codeva-1hikzyFV3T" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no, shrink-to-fit=no" />
