1f1ed400167756a433d5cda72b66344be3483ab9
ianyscript <133550607+ianyscript@users.noreply.github.com>
2023-06-05 10:23:54 +0000

    fix

--- src/html.js
+++ src/html.js
@@ -18,7 +18,7 @@ const HTML = function ({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no, shrink-to-fit=no" />
-        <link rel="stylesheet" href="https://npm.elemecdn.com/lxgw-wenkai-screen-webfont/style.css" media="print" onload="this.media='all'"></link>
+        <link rel="stylesheet" href="https://npm.elemecdn.com/lxgw-wenkai-screen-webfont/style.css"></link>
 
         {headComponents}
       </head>
